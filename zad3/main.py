8daf8dcc8daecb8a83cc158768b200e820ce5d7c
crppl <janmachowski69@gmail.com>
2025-06-03 14:20:01 +0200

    looks good

--- zad3/main.py
+++ zad3/main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import math
 import time
+from functools import cache
 
 
 
@@ -9,6 +10,12 @@ def aks():
 
 HEIGHT = 600
 WIDTH = 600
+
+W2 = WIDTH/2
+H2 = HEIGHT/2
+
+# RW = 1/WIDTH
+# RH = 1/HEIGHT
 
 # GUI begin
 pg.init()
@@ -46,10 +53,10 @@ class Ball:
         self.n = n
         self.color = color
 
-light_source = LSource([0, 0, 1000], [1.0, 1.0, 1.0])
+light_source = LSource([0, 0, 0], [1.0, 1.0, 1.0])
 light_source.setImage("zad3/sun.png")
 
-balls = [Ball([WIDTH//4,HEIGHT//4,0], int(min(WIDTH//4, HEIGHT//4)), 64, (0.7, 0, 0.7))]
+balls = [Ball([WIDTH//4,HEIGHT//4,0], int(min(WIDTH//4, HEIGHT//4)), 200, (1.0, 0, 1.0))]
 ball_surfs = []
 
 def ftoc(a:float):
@@ -57,6 +64,10 @@ def ftoc(a:float):
 
 def vlen(vect):
     return math.sqrt(vect[0]**2 + vect[1]**2 + vect[2]**2)
+
+def normalize(vect):
+    v = vlen(vect)
+    return [vect[0]/v, vect[1]/v, vect[2]/v], v
 
 def update():
 
@@ -75,7 +86,7 @@ def update():
             light_source.move(dy=-move_speed)
         if pg.K_ESCAPE in keys_pressed:
             return 
-        
+
         global ball_surfs, balls
         ball_surfs = []
         for ball in balls:
@@ -83,44 +94,49 @@ def update():
             for i in range(-ball.radius, ball.radius,2):
                 for j in range(-ball.radius, ball.radius,2):
                     if i**2 + j**2 <= (ball.radius)**2:
+                        iB = i + ball.radius
+                        jB = j + ball.radius
                         # basics
-                        z = math.sqrt(ball.radius**2 - i**2 - j**2)
-                        pkt_sc = [i+ball.start[0]+ball.radius, j+ball.start[1]+ball.radius, z] # faktyczne współrzędne punktu na scenie
-                        L = [light_source.pos[0] - pkt_sc[0], light_source.pos[1] - pkt_sc[1], light_source.pos[2] - pkt_sc[2]]
-                        mL = 0
-                        for x in L:
-                            if abs(x) > abs(mL):
-                                mL = x
-                        light_dir = [x/mL if mL != 0 else 0 for x in L ]
+                        z = math.sqrt((ball.radius)**2 - i**2 - j**2)
+                        # to powinno mieć dużo sensu
+                        L = [(light_source.pos[0] - ball.start[0]) - iB, (light_source.pos[1] - ball.start[1]) - jB, light_source.pos[2] - z]# - z-ball.start[0]]#-ball.radius]
 
-                        mN = 0
-                        for x in [i,j,z]:
-                            if abs(x) > abs(mN):
-                                mN = x
-
-                        normal = [x/mN if mN != 0 else 0 for x in [i,j,z]]
-                        print(normal)
-
+                        normal, vN = normalize([i, j, z])
+                        # normalna zakładając, że środek kuli to (rad,rad,z)
+                        light_dir, vL = normalize(L)
+                        
                         # diffuse
                         # spójrz tu na normalną
-                        tmp = max(0.0, dot([light_dir[0], light_dir[1], light_dir[2]], normal))
+                        tmp = dot(normal, light_dir) # dNL
+                        # tmp = max(0.0, dNL)
+
                         diff = [tmp*light_source.color[0], tmp*light_source.color[1], tmp*light_source.color[2]]
 
                         # specular
-                        
-                        cT = dot(normal, light_dir) / (vlen(normal) *vlen(light_dir)) #L
-                        V = [light_dir[0] - 2 * normal[0]*cT, light_dir[1]-2*normal[1]*cT,light_dir[2]-2*normal[2]*cT]
-                        cA = max(0.0, (dot(V, [0.0,0.0,-1.0]) / (vlen(V))))**(ball.n)
+                        cT = tmp / (vN *vL) #dNL
+                        R, vR = normalize([light_dir[0] - 2 * normal[0]*cT, light_dir[1]-2*normal[1]*cT,light_dir[2]-2*normal[2]*cT])
+                        # cA = max(0.0, (dot(V, [0.0,0.0,1.0]) / vN))**(ball.n)
+                        V, vV = normalize([W2 - ball.start[0] - iB, H2 - ball.start[1] - jB , z - 1000])
+                        cA = dot(R, V)**(ball.n)
                         spec = [cA*light_source.color[0], cA*light_source.color[1], cA*light_source.color[2]]
 
                         # suma
-                        clr = [ftoc(color*0.3+ df*0.7 + 0.0*sp) for (color, df, sp) in zip(ball.color, diff, spec)]
-                        pg.draw.circle(ball_surf, clr, (i+ball.radius,j+ball.radius), 2)
+                        clr = [ftoc(color*0.2 + df*0.25 + 0.75*sp) for (color, df, sp) in zip(ball.color, diff, spec)]
+                        pg.draw.circle(ball_surf, clr, (iB,jB), 2)
 
             ball_surfs.append((ball_surf, (ball.start[0], ball.start[1])))
 
 
 keys_pressed = set()
+
+
+font = pg.font.Font(pg.font.get_default_font(), 20)
+
+def updateText():
+    global light_source
+    string = "x: " + str(light_source.pos[0]) + " y: " + str(light_source.pos[1]) + " z: " + str(light_source.pos[2]) 
+    return font.render(string, True, "#FFFFFF")
+
 
 #GUI loop
 move_speed=10
@@ -129,7 +145,14 @@ while running:
     screen.fill((0,0,0))
     screen.blits(ball_surfs)
     if light_source.img is not None:
-        screen.blit(light_source.img, (light_source.pos[0]-15, light_source.pos[1]-15))
+        screen.blit(light_source.img, (light_source.pos[0] - 15, light_source.pos[1] - 15))
+    
+
+    # pg.draw.rect(screen, [255,255,255], (200,100,100,100))
+    text = updateText()
+    textR = text.get_rect()
+    screen.blit(text, textR)
+
     pg.display.flip()
 
     update()            
